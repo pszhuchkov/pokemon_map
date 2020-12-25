8e6e0f0fdaaad75cbba4d72811ae17ba9e9fa15f
pzhuchkov <pzhuchkov@gmail.com>
2020-12-25 11:27:44 +0300

    Disable null value for Charfield

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Pokemon(models.Model):
     title_ru = models.CharField('название (рус.)', max_length=200)
     title_en = models.CharField(
-        'название (англ.)', max_length=200, null=True, blank=True
+        'название (англ.)', max_length=200, blank=True
     )
     title_jp = models.CharField(
-        'название (яп.)', max_length=200, null=True, blank=True
+        'название (яп.)', max_length=200, blank=True
     )
     image = models.ImageField('изображение', null=True, blank=True)
     description = models.TextField('описание', blank=True)
